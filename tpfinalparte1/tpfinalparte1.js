19b975bdd216bb2283ade43741113f7e621549bd
ianurbanski <166758728+ianurbanski@users.noreply.github.com>
2024-10-19 23:40:24 -0300

    tpfinalparte1

---
+++ tpfinalparte1/tpfinalparte1.js
@@ -0,0 +1,119 @@
+/*nombre: ian urbanski  120379/3
+trabajo hecho en conjunto con juan ignacio ventimiglia
+link video youtube   https://youtu.be/DqcbGJgCQ34
+*/
+
+
+let imagenes = [];
+let texto = [];
+let indiceImagen = 0;
+let sonido;
+let botonActivo = true;
+
+function preload() {
+  for (let i = 0; i < 21; i++) {
+    let nombreArchivo = 'data/' + i + '.jpg';
+    imagenes[i] = loadImage(nombreArchivo);
+  }
+  sonido = loadSound('data/sonido1.mp3');
+}
+
+function setup() {
+  createCanvas(640, 480);
+  cargarTexto();
+  textWrap(WORD); 
+  sonido.setVolume(0.05);
+  frameRate(10);
+
+}
+
+function draw() {
+  background(220);
+
+  
+  image(imagenes[indiceImagen], 0, 0, 640, 480);
+
+ 
+  if (indiceImagen === 0 && botonActivo) {
+    botonSonido();
+  }
+
+  // Dibujar un rectángulo semitransparente para el texto
+  stroke(255);
+  fill(150, 200);
+  rect(0, 360, 640, 119);
+
+  fill(255);
+  noStroke();
+  textSize(16);
+  textAlign(LEFT, CENTER);
+  
+  let margen = 20;
+  let yCentrado = 360 + 119 / 2;
+  
+  text(texto[indiceImagen], margen, yCentrado - 55, width - 2 * margen, 120);
+
+  if (indiceImagen === 6 || indiceImagen === 17) {
+    botonesDecision();
+  } else if (indiceImagen === 20) {
+    botonReinicio();
+  } else {
+    botonPasar();
+  }
+
+  
+  if (indiceImagen === 20 && sonido.isPlaying()) {
+    sonido.stop();
+  }
+
+  if (indiceImagen === 20) {
+    fill(255);
+    textAlign(CENTER, CENTER);
+    text(texto[21], 329, 390);
+  }
+}
+
+function iniciarSonido() {
+  if (!sonido.isPlaying()) {
+    sonido.play();
+    botonActivo = false; 
+  }
+}
+
+function mousePressed() {
+  
+  if (indiceImagen === 0 && botonActivo &&
+      mouseX > 270 && mouseX < 370 && mouseY > 50 && mouseY < 90) {
+    iniciarSonido();
+  }
+
+  
+  if (indiceImagen !== 6 && indiceImagen !== 17 && indiceImagen !== 20 && mouseX > 295 && mouseX < 345 && mouseY > 215 && mouseY < 265) {
+    indiceImagen = (indiceImagen === 8 || indiceImagen === 18) ? 20 : indiceImagen + 1;
+  }
+
+  // Bifurcación 1
+  if (indiceImagen === 6) {
+    if (mouseX > 40 && mouseX < 140 && mouseY > 220 && mouseY < 260) {
+      indiceImagen = 7;
+    } else if (mouseX > 500 && mouseX < 600 && mouseY > 220 && mouseY < 260) {
+      indiceImagen = 9;
+    }
+  }
+
+  // Bifurcación 2
+  if (indiceImagen === 17) {
+    if (mouseX > 40 && mouseX < 140 && mouseY > 220 && mouseY < 260) {
+      indiceImagen = 18;
+    } else if (mouseX > 500 && mouseX < 600 && mouseY > 220 && mouseY < 260) {
+      indiceImagen = 19;
+    }
+  }
+
+  // Reinicio de la aventura
+  if (indiceImagen === 20 && mouseX > 279 && mouseX < 379 && mouseY > 280 && mouseY < 320) {
+    indiceImagen = 0;
+    botonActivo = true;
+    sonido.stop();
+  }
+}
